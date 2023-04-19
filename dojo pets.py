7d13f38baba137765e0cdd27a3b0f5a88d0c9615
inbumbaek <119047796+inbumbaek@users.noreply.github.com>
2023-04-19 13:15:26 -0700

    Add files via upload

---
+++ dojo pets.py
@@ -0,0 +1,50 @@
+class Ninja:
+    def __init__(self, first_name, last_name, treats, pet_food, pet):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.treats = treats
+        self.pet_food = pet_food
+        self.pet = pet
+
+# walk() - walks the ninja's pet invoking the pet play() method
+    def walk(self):
+        self.pet.play()
+        return self
+    
+# feed() - feeds the ninja's pet invoking the pet eat() method
+    def feed(self):
+        self.pet.eat()
+        return self
+
+# bathe() - cleans the ninja's pet invoking the pet noise() method
+    def bathe(self):
+        self.pet.noise()
+        return self
+        
+class Pet:
+    def __init__(self, name, type, tricks):
+        self.name = name
+        self.type = type
+        self.tricks = tricks
+        self.health = 100
+        self.energy = 100
+
+# sleep() - increases the pets energy by 25
+    def sleep(self):
+        self.energy += 25
+        return self
+
+# eat() - increases the pet's energy by 5 & health by 10
+    def eat(self):
+        self.energy += 5
+        self.health += 10
+        return self
+
+# play() - increases the pet's health by 5
+    def play(self):
+        self.health += 5
+        return self
+    
+# noise() - prints out the pet's sound
+    def noise(self):
+        print(self.noise)
